e5948809021fbe9980cfb9bdea5384f89f95fd1e
Neil Goodridge <neilrgoodridge@gmail.com>
2022-05-16 17:53:15 +0100

    removed extra and from drivers controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -8,13 +8,4 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
-  url = URI("http://ergast.com/api/f1/drivers?=123")
-
-  http = Net::HTTP.new(url.host, url.port);
-  request = Net::HTTP::Get.new(url)
-
-  response = http.request(request)
-  puts response.read_body
-
-  end
 end
